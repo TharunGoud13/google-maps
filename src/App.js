625f89748627cab5608b0d2bf772228a71a49e5e
Tharun kumar goud <amaravaitharun13@gmail.com>
2024-03-31 12:03:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        {/* <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow> */}
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCHdKlGxI_QfZIg1ReYssnyuYtAweqdXiU",
+})(MapContainer);
